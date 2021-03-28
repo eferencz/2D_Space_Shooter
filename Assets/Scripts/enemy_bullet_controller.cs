46a508b7649beefe398230f1c9ed571f3591c3e3
eferencz <ferencz@chapman.edu>
2021-03-27 18:57:02 -0700

    Initial commit

---
+++ Assets/Scripts/enemy_bullet_controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_bullet_controller : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, speed);
+    }
+
+    void Update()
+    {
+        if (Camera.main.WorldToViewportPoint(transform.position).y < 0)
+            Destroy(this.gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject.Destroy(this.gameObject);
+            GameObject.Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# 2D_Space_Shooter
+ 
